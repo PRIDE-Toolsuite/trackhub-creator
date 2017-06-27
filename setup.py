a2ca2800f21122f30419342b3148781f6cae6a63
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-27 13:44:03 +0100

    setup information

--- setup.py
+++ setup.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 # TODO I'm not sure I'll use this setup anyway
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='trackhub-creator',
+    version='1.0.0',
+    description='This is a pipeline to generate trackhubs for Ensembl, from either PRIDE Cluster or single projects',
+    long_description=readme,
+    author='Manuel Bernal-Llinares',
+    author_email='mbdebian@gmail.com',
+    url='https://github.com/PRIDE-Toolsuite/trackhub-creator',
+    license=license,
+    packages=find_packages(exclude('tests', 'docs'))
+)
