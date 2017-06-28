51cee779abed3b1ead964783e706f9bf2495a9cf
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 10:30:51 +0100

    logging default configuration

--- configManager.py
+++ configManager.py
@@ -21,9 +21,18 @@ import importlib
 # Package modules
 from exceptions import ConfigException
 
+
 # Application defaults - NORMAL OPERATION MODE
 _folder_bin = os.path.abspath('bin')
 _folder_config = os.path.abspath('config')
 _folder_docs = os.path.abspath('docs')
 _folder_logs = os.path.abspath('logs')
 _folder_resources = os.path.abspath('resources')
+
+
+# Logging defaults
+_logger_formatters = {
+	"DEBUG": "%(asctime)s [%(levelname)7s][%(name)48s][%(module)18s, %(lineno)4s] %(message)s",
+	"INFO": "%(asctime)s [%(levelname)7s][%(name)48s] %(message)s"
+}
+_log_level = 'DEBUG'
