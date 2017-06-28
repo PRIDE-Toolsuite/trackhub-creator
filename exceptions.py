7199c7e3cdd18a239ffc3a7987e935e4330a4c7a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 10:17:00 +0100

    application wide exceptions module placeholder

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 28-06-2017 10:16
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Application wide exceptions
+"""
+
