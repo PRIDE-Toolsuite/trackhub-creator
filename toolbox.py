001680e67dcf80ec251786448228db8e6f22479b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 11:05:20 +0100

    pipeline runner toolbox placeholder

---
+++ toolbox.py
@@ -0,0 +1,13 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 28-06-2017 11:03
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+This module implements some useful functions for the pipeline runner
+"""
+
