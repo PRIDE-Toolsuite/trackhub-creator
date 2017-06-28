4620964580449601d5859353c7c2d119361f2f51
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 10:28:46 +0100

    initial application defaults

--- configManager.py
+++ configManager.py
@@ -11,3 +11,19 @@
 This module manages the configuration for the running pipeline
 """
 
+
+# System modules
+import os
+import time
+import json
+import logging
+import importlib
+# Package modules
+from exceptions import ConfigException
+
+# Application defaults - NORMAL OPERATION MODE
+_folder_bin = os.path.abspath('bin')
+_folder_config = os.path.abspath('config')
+_folder_docs = os.path.abspath('docs')
+_folder_logs = os.path.abspath('logs')
+_folder_resources = os.path.abspath('resources')
