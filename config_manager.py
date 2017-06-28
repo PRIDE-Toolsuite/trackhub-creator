a7ee2615a57badbefcbc3939d598ad7e508be7d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 11:39:45 +0100

    get/set pipeline name

--- config_manager.py
+++ config_manager.py
@@ -30,6 +30,7 @@ _folder_docs = os.path.abspath('docs')
 _folder_logs = os.path.abspath('logs')
 _folder_resources = os.path.abspath('resources')
 __configuration_file_name = None
+__pipeline_name = None
 
 
 # Logging defaults
@@ -43,7 +44,8 @@ _log_level = 'DEBUG'
 def set_application_config_file(configuration_file):
 	"""
 	This method sets the application wide configuration file that will be used
-	:param configuration_file: config file name if the file is in the default configuration path or path to the configuration file if it is not
+	:param configuration_file: config file name if the file is in the default configuration path or path to the
+	configuration file if it is not.
 	:return: no return value
 	:exception: ConfigException is raised if there already is a configuration file set
 	"""
@@ -51,6 +53,23 @@ def set_application_config_file(configuration_file):
 	if __configuration_file_name is not None:
 		raise ConfigException("Configuration file can't be changed once an initial configuartion file has been provided")
 	__configuration_file_name = configuration_file
+
+
+def set_pipeline_name(pipeline_name):
+	"""
+	Set the name of the pipeline being run, once it is set, it can't be changed, no matter how many times this method is
+	called.
+	:param pipeline_name: pipeline name
+	:return: the current pipeline name
+	"""
+	global __pipeline_name
+	if __pipeline_name is None:
+		__pipeline_name = pipeline_name
+	return __pipeline_name
+
+
+def get_pipeline_name():
+	return set_pipeline_name("no_pipeline_name_specified")
 
 
 # Configuration Singleton
@@ -62,5 +81,13 @@ def get_app_config_manager():
 	pass
 
 
-class AppConfigManager:
+def __check_create_folders(configuration_object):
+	# TODO
 	pass
+
+
+class AppConfigManager:
+	def __init__(self, configuration_object):
+		self.__configuration_object = configuration_object
+		# TODO check and create folders (if needed)
+		self.__session_id = time.strftime('%Y.%m.%d_%H.%M') + "-" + get_pipeline_name()
