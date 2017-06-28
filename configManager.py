fbe08703ff93e11a6f3210aa5034c6606792b908
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 09:51:34 +0100

    new configManager placeholder

---
+++ configManager.py
@@ -0,0 +1,13 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 28-06-2017 9:50
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+This module manages the configuration for the running pipeline
+"""
+
