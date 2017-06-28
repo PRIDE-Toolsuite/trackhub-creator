d2df6f5057267542876e2827e6f09e1fafba0663
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-28 16:11:27 +0100

    get folder for binaries

--- config_manager.py
+++ config_manager.py
@@ -153,6 +153,10 @@ class AppConfigManager(ConfigurationManager):
         # Configuration folder cannot be changed in this version of the pipeline
         return _folder_config
 
+    def get_folder_bin(self):
+        # 'Bin' folder cannot be changed in this version of the pipeline
+        return _folder_bin
+
     def get_logger(self):
         # TODO
         pass
