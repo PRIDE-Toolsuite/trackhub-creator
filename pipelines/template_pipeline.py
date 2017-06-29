cc2e1799fea22355d1b43142ec5f632dfa724ebd
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-29 15:09:11 +0100

    template for pipelines, for refactoring out later on

---
+++ pipelines/template_pipeline.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 29-06-2017 15:05
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+This is a template pipeline for refactoring out things from final pipelines as I identify how they're gonna look like
+"""
