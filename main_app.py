80f81a1f996af2f9c4dab6df00fe29ce4ea1d4df
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-29 11:48:25 +0100

    main application placeholder

---
+++ main_app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 29-06-2017 11:47
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Pipeline Runner - Main Application
+"""
