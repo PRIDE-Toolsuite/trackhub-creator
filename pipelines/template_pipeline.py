1905d55b405e2ac73f5741ead8c4da9fa4eb200f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-29 15:16:50 +0100

    skeleton of a pipeline director

--- pipelines/template_pipeline.py
+++ pipelines/template_pipeline.py
@@ -10,3 +10,24 @@
 """
 This is a template pipeline for refactoring out things from final pipelines as I identify how they're gonna look like
 """
+
+
+class Director():
+    """
+    This is the director of the pipeline
+    """
+    def __init__(self, config_file_name, runner_id = 0):
+        #TODO
+        pass
+
+    def _before(self):
+        # TODO
+        pass
+
+    def run(self):
+        # TODO
+        pass
+
+    def _after(self):
+        # TODO
+        pass
