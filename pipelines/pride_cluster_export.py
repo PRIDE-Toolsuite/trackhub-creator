85757fe5efad7bceb681061a7f65ccde3a40b7c3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-01 23:23:33 +0100

    PEP 8 compliance

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -15,7 +15,6 @@ PoGo formatted files for its later use
 import config_manager
 import toolbox
 
-
 __configuration_file = None
 __configuration_manager = None
 
@@ -23,13 +22,15 @@ __configuration_manager = None
 def __get_configuration_manager():
     global __configuration_manager
     if __configuration_manager is None:
-        __configuration_manager = DirectorConfigurationManager(toolbox.read_json(__configuration_file), __configuration_file)
+        __configuration_manager = DirectorConfigurationManager(toolbox.read_json(__configuration_file),
+                                                               __configuration_file)
     return __configuration_manager
 
 
 class DirectorConfigurationManager(config_manager.ConfigurationManager):
     def __init__(self, configuration_object, configuration_file):
         super(DirectorConfigurationManager, self).__init__(configuration_object, configuration_file)
+
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not met to be run in stand alone mode")
