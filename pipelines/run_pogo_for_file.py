5019d36007191aebae90b758bf933ee1fd32982d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-01 23:24:15 +0100

    prevent script from running in standalone mode

--- pipelines/run_pogo_for_file.py
+++ pipelines/run_pogo_for_file.py
@@ -11,3 +11,6 @@
 This module runs PoGo for a file, using the given GTF and FA reference files, it will produce the results in the same
 folder where the input file is located
 """
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a pipeline collection and it is not met to be run in stand alone mode")
