5f5b562a6eca7bc3eebe4043bcf83af9bb5d957f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-01 23:26:58 +0100

    module specific config manager singleton get method

--- pipelines/run_pogo_for_file.py
+++ pipelines/run_pogo_for_file.py
@@ -19,6 +19,12 @@ import toolbox
 __configuration_file = None
 __configuration_manager = None
 
+def __get_configuration_manager():
+    global __configuration_manager
+    if __configuration_manager is None:
+        __configuration_manager = DirectorConfigurationManager(toolbox.read_json(__configuration_file),
+                                                               __configuration_file)
+    return __configuration_manager
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not met to be run in stand alone mode")
