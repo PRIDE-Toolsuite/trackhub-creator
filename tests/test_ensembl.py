2d33251daee62609f515453b7e79275e7c377bff
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-03 15:26:06 +0100

    Doh! I was mixing in the old style of formatting strings in python, in a wrong way

--- tests/test_ensembl.py
+++ tests/test_ensembl.py
@@ -32,7 +32,7 @@ class TestEnsemblService(unittest.TestCase):
     def test_get_ensembl_current_release(self):
         service = EnsemblService(config_manager.read_config_from_file(self.__CONFIG_FILE_NAME), self.__CONFIG_FILE_NAME)
         current_release_number = service.get_release_number()
-        print("Current release number ---> {}" % str(current_release_number))
+        print("Current release number ---> {}".format(current_release_number))
 
 
 if __name__ == '__main__':
