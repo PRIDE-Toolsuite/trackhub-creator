a4647d7b2f42e1e9a1bc7b9489ecf3ac3326b9c1
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 09:14:59 +0100

    unit tests for ensembl species service placeholder

---
+++ tests/test_ensembl_species_service.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 04-07-2017 09:14
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
