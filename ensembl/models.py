e2caee8a72bf8bd3d33598b6d9e285892754637b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 10:09:47 +0100

    trying with lambda

--- ensembl/models.py
+++ ensembl/models.py
@@ -85,7 +85,7 @@ class SpeciesService:
         :param property_getter: property on which the index should be created
         :return: a dictionary of the given data objects where the key is the indexed property
         """
-        return {property_getter.__get__(data_item, Species)(): data_item for data_item in data}
+        return {(lambda *args, **kwargs: property_getter(data_item, *args, **kwargs))(): data_item for data_item in data}
 
     def _get_logger(self):
         return self.__logger
