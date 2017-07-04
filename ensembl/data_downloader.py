b8ac7e06f35cec7b2c33d0e2d08fd39193d2595b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 15:42:08 +0100

    get_folder_name_fasta config method

--- ensembl/data_downloader.py
+++ ensembl/data_downloader.py
@@ -80,6 +80,20 @@ class ConfigurationManager(config_manager.ConfigurationManager):
                     self._CONFIG_KEY_FOLDER_PREFIX_RELEASE,
                     self._get_configuration_file()))
 
+    def get_folder_name_fasta(self):
+        try:
+            return self._get_configuration_object()\
+                [self._CONFIG_KEY_DATA_DOWNLOADER]\
+                [self._CONFIG_KEY_ENSEMBL_FTP]\
+                [self._CONFIG_KEY_FOLDER_NAME_FASTA]
+        except Exception as e:
+            raise ConfigManagerException(
+                "MISSING configuration information '{}.{}.{}' in configuration file '{}'".format(
+                    self._CONFIG_KEY_DATA_DOWNLOADER,
+                    self._CONFIG_KEY_ENSEMBL_FTP,
+                    self._CONFIG_KEY_FOLDER_NAME_FASTA,
+                    self._get_configuration_file()))
+
 
 class DataDownloadService:
     """
