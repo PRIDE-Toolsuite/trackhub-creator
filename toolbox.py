18c4625523e1efe34eafd5ce46dd6a6a32ac517b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 08:19:23 +0100

    typo fixed

--- toolbox.py
+++ toolbox.py
@@ -46,4 +46,4 @@ def check_create_folders(folders):
 
 
 if __name__ == '__main__':
-    print("ERROR: This script is part of a pipeline collection and it is not met to be run in stand alone mode")
+    print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
