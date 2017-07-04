2bac2e6b684ef3205d53bb89578ffd5f0f1a1f3c
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 11:04:28 +0100

    download manager configuration manager singleton

--- ensembl/data_downloader.py
+++ ensembl/data_downloader.py
@@ -28,5 +28,18 @@ def set_configuration_file(config_file):
     return __configuration_file
 
 
+def get_configuration_manager():
+    global __configuration_manager
+    if __configuration_manager is None:
+        __configuration_manager = ConfigurationManager(config_manager.read_config_from_file(__configuration_file),
+                                                       __configuration_file)
+    return __configuration_manager
+
+
+# Ensembl Download Manager configuration manager
+class ConfigurationManager(config_manager.ConfigurationManager):
+    pass
+
+
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
