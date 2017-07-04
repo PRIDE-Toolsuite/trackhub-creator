82956f6c7bc4cacd87770e9f3f65bd15718fbc9b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 08:18:11 +0100

    typo fixed

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -46,4 +46,4 @@ class Director(template_pipeline.Director):
 
 
 if __name__ == '__main__':
-    print("ERROR: This script is part of a pipeline collection and it is not met to be run in stand alone mode")
+    print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
