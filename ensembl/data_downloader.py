e77233e5afe6a44590c1294d7807132c04264425
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 11:05:54 +0100

    logger getter

--- ensembl/data_downloader.py
+++ ensembl/data_downloader.py
@@ -42,6 +42,9 @@ class ConfigurationManager(config_manager.ConfigurationManager):
         super(ConfigurationManager, self).__init__(configuration_object, configuration_file)
         self.__logger = config_manager.get_app_config_manager().get_logger_for(__name__)
 
-        
+    def _get_logger(self):
+        return self.__logger
+
+
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
