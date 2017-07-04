b83835ebd639e5f6c0854e45c9b17bd93c60a791
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-04 08:18:00 +0100

    prevent the script from running standalone

--- pipelines/exceptions.py
+++ pipelines/exceptions.py
@@ -16,3 +16,8 @@ from exceptions import AppException
 class PipelineDirectorException(AppException):
     def __init__(self, value):
         super(AppException, self).__init__(value)
+
+
+if __name__ == '__main__':
+    print(
+        "ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
