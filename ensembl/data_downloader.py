f479596776fd0f8dec900b9885b92704eabe358f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-05 14:52:38 +0100

    local path for current ensembl release

--- ensembl/data_downloader.py
+++ ensembl/data_downloader.py
@@ -191,6 +191,7 @@ class DataDownloadService:
         self._get_logger().debug("Using configuration file '{}'".format(configuration_file))
         self.__config_manager = ConfigurationManager(configuration_object, configuration_file)
         self.__local_path_ensembl_repo = None
+        self.__local_path_ensembl_release = None
         # Name for the current release
         self.__ensembl_release_name = None
         # Name for the subfolder that contains per species fasta files
