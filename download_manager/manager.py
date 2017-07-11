4b0007a6f11fc518393a0cbe439f3f80bc378a5b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-11 07:43:08 +0100

    PEP 8 compliant

--- download_manager/manager.py
+++ download_manager/manager.py
@@ -208,7 +208,8 @@ class Agent(threading.Thread):
 
 
 class Manager:
-    def __init__(self, urls, download_destination_folder, logger, download_attempts=32, timeout_attempts=3, download_timeout=120):
+    def __init__(self, urls, download_destination_folder, logger, download_attempts=32, timeout_attempts=3,
+                 download_timeout=120):
         self.__urls = urls
         self.__download_destination_folder = download_destination_folder
         self.__logger = logger
