2ea8e27235dc6d6471dc484638b391d704695a36
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-11 10:40:57 +0100

    download manager unit tests placeholder

---
+++ tests/test_download_manager.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 11-07-2017 10:40
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
