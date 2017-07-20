78532333845302cae8aae016db5edda34e924e68
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-20 09:33:07 +0100

    refactor getter, log handlers

--- config_manager.py
+++ config_manager.py
@@ -193,6 +193,9 @@ class AppConfigManager(ConfigurationManager):
         self._get_logger().debug("Logging system initialized")
         # TODO to be completed
 
+    def _get_log_handlers(self):
+        return self.__log_handlers
+
     def get_folder_bin(self):
         # 'Bin' folder cannot be changed in this version of the pipeline
         return _folder_bin
@@ -215,9 +218,6 @@ class AppConfigManager(ConfigurationManager):
 
     def get_session_working_dir(self):
         return self.__session_working_dir
-
-    def _get_log_handlers(self):
-        return self.__log_handlers
 
     def get_logger_for(self, name):
         """
