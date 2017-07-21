1335782c7691d57371b5743f2a319e17eb6b4214
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-21 14:29:37 +0100

    typo, and calculate file name

--- tests/test_toolboxes.py
+++ tests/test_toolboxes.py
@@ -19,8 +19,10 @@ import toolbox.general as general_toolbox
 
 class TestToolboxes(unittest.TestCase):
     __logger = config_manager.get_app_config_manager().get_logger_for(__name__)
-    
+
     def test_gunzip_files(self):
-        file_url = 'ftp://ftp.ensembl.org/pub/release-89/gtf/homo_sapiens/Homo_sapiens.GRCh38.89.abinitio.gtf.gz';
+        file_url = 'ftp://ftp.ensembl.org/pub/release-89/gtf/homo_sapiens/Homo_sapiens.GRCh38.89.abinitio.gtf.gz'
+        file_name = file_url[file_url.rfind('/') + 1:]
         # Download the file to the session working directory
         destination_folder = config_manager.get_app_config_manager().get_session_working_dir()
+
