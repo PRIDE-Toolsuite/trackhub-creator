68692e1dd1bb6a75a7367705f9f5b346f8c62263
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-28 10:14:51 +0100

    attribute for pipeline director singleton

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -19,7 +19,7 @@ from pipelines.template_pipeline import Director, DirectorConfigurationManager
 
 __configuration_file = None
 __pipeline_arguments = None
-
+__pipeline_director = None
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
