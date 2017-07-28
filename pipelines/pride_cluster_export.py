212c3be4b58849f3a8c9f04c5270d84615e68d68
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-28 10:14:11 +0100

    config file attribute

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -17,5 +17,7 @@ import config_manager
 import ensembl
 from pipelines.template_pipeline import Director, DirectorConfigurationManager
 
+__configuration_file = None
+
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
