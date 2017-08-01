8a2234e5b9178b9bda41ebb70de6afcce764d4c7
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-01 16:36:04 +0100

    get_genome_reference_for_species deal with 'not found' taxonomy

--- ensembl/data_downloader.py
+++ ensembl/data_downloader.py
@@ -680,6 +680,8 @@ class DataDownloadService:
     def get_genome_reference_for_species(self, taxonomy_id):
         # Work out the file names for the data to retrieve from Ensembl
         file_names = self._get_genome_reference_ensembl_file_name_for_species(taxonomy_id)
+        if not file_names:
+            return None
         self._get_logger().debug("Working with Ensembl GTF file names for taxonomy ID '{}' - '{}'"
                                  .format(taxonomy_id, str(file_names)))
         # Work out their path in the local repository
