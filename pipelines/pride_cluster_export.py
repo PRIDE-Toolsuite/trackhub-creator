d5ad8c9f32f9d29475739a748317aa5618dbbcd6
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-02 15:08:04 +0100

    add timing capabilities to PoGo command

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -353,7 +353,7 @@ class PrideClusterExporter(Director):
                                     .format(pogo_parameter_file_input,
                                             pogo_parameter_protein_sequence_file_path,
                                             pogo_parameter_gtf_file_name))
-            pogo_command = "{} -fasta {} -gtf {} -in {}" \
+            pogo_command = "time {} -fasta {} -gtf {} -in {}" \
                 .format(self._get_configuration_manager().get_pogo_binary_file_path(),
                         pogo_parameter_protein_sequence_file_path,
                         pogo_parameter_gtf_file_name,
