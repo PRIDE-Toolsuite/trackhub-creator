42888f5edc04afbbd5f1e21f2a77157bb13f3c08
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-02 16:40:24 +0100

    models for dealing with PoGo

---
+++ pogo/models.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 02-08-2017 16:40
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
