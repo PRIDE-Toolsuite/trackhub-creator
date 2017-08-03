aa5c2484004bd7efef74a349cc44d70a0724278d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-03 12:13:33 +0100

    configuration manager for pogo module, placeholder

---
+++ pogo/config_manager.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 03-08-2017 12:13
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
