b0bc2411b7a4308a2bfbdad46b3a636df2597d5f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-03 12:33:22 +0100

    ConfigurationService skeleton

--- pogo/config_manager.py
+++ pogo/config_manager.py
@@ -11,7 +11,12 @@
 Configuration manager for this module
 """
 
+# Application imports
+import config_manager
+
 # TODO - Every module should have a configuration manager like this, refactor in the future, whenever you have time
+# WARNING - Yes, I know, I've always called this 'Configuration Manager' and now I'm calling it 'Configuration Service',
+# well, welcome to the wonderful world of 'I've changed my mind' in the land of 'refactoring' ^_^
 
 
 # Configuration file and singleton instance
@@ -24,3 +29,9 @@ def set_configuration_file(config_file):
     if __configuration_file is None:
         __configuration_file = config_file
     return __configuration_file
+
+
+class ConfigurationService(config_manager.ConfigurationManager):
+    def __init__(self, configuration_object, configuration_file):
+        super(ConfigurationService, self).__init__(configuration_object, configuration_file)
+        self.__logger = config_manager.get_app_config_manager().get_logger_for(__name__)
