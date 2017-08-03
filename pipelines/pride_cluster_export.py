1d36abaa73bac4217facf593b8b81db3d9c2916e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-03 11:29:52 +0100

    helper placeholder for refactoring

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -305,6 +305,9 @@ class PrideClusterExporter(Director):
             return False
         return True
 
+    def __get_pogo_protein_sequence_for_taxonomy(self, taxonomy_id):
+        pass
+
     def __run_pogo_on_pride_cluster_file_exporter_results(self, cluster_file_exporter_result_mapping):
         # Prepare results object, it is a map like (taxonomy_id, PogoRunResult)
         pogo_run_results = {}
