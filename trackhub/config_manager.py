c0fb7024adf7edf1b778d4eb18ba3d8990c71736
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-17 11:53:03 +0100

    prevent the script from running in standalone mode

--- trackhub/config_manager.py
+++ trackhub/config_manager.py
@@ -35,4 +35,12 @@ def get_configuration_service():
 
 
 class ConfigurationService(config_manager.ConfigurationManager):
-    pass
+    # This is similar code to the one I have for other modules, but this time I'm trying the 'pythonist way' to get a
+    # feeling of it
+    def __init__(self, configuration_object, configuration_file):
+        super(ConfigurationService, self).__init__(configuration_object, configuration_file)
+        self.logger = config_manager.get_app_config_manager().get_logger_for(__name__)
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
