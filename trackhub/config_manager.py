c129ebef0457b4f8af4f0a9cd6d60ca8dc780958
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-17 11:41:34 +0100

    configuration manager (placeholder) for trackhub module

---
+++ trackhub/config_manager.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 17-08-2017 11:40
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
