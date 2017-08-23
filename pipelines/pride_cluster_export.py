332ff79d0fe6557a72f1862658a9a9890ac62662
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-23 12:30:32 +0100

    get_trackhub_registry_url placeholder

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -137,6 +137,9 @@ class ConfigManager(DirectorConfigurationManager):
     def get_url_pride_cluster_trackhubs(self):
         pass
 
+    def get_trackhub_registry_url(self):
+        pass
+
     def get_cluster_file_exporter_version_parameter(self):
         """
         This method computes the 'version' parameter value for running pride cluster-file-exporter software, it usually
