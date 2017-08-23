74b58c8eec05b492bf4b009ce6d59b48c1d5a394
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-23 14:14:19 +0100

    set the trackhub destination folder at the trackhub exporter

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -635,6 +635,7 @@ class PrideClusterExporter(Director):
                 self._get_configuration_manager().get_cluster_file_exporter_version_parameter())
             general_toolbox.check_create_folders([trackhub_destination_folder])
             general_toolbox.create_latest_symlink_overwrite(trackhub_destination_folder)
+            trackhub_exporter.track_hub_destination_folder = trackhub_destination_folder
             self._get_logger().info("PRIDE Cluster trackhub destination folder '{}' prepared")
         else:
             self._get_logger().warning("TODO - USING DEFAULT TRACKHUB DESTINATION FOLDER '{}'".format(
