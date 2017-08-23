6ba712f45c1df2c5b3bda92ee0c9bd5659b90a95
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-23 11:24:29 +0100

    _CONFIG_COMMAND_LINE_ARGUMENT_KEY_FOLDER_PRIDE_CLUSTER_TRACKHUGS configuration key

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -61,6 +61,8 @@ class ConfigManager(DirectorConfigurationManager):
     # Configuration keys for PoGo
     _CONFIG_POGO_BIN_SUBFOLDER = "pogo"
     _CONFIG_POGO_BIN_FILE_NAME = "pogo"
+    # Command line arguments
+    _CONFIG_COMMAND_LINE_ARGUMENT_KEY_FOLDER_PRIDE_CLUSTER_TRACKHUGS = 'folder_pride_cluster_trackhubs'
 
     def __init__(self, configuration_object, configuration_file, pipeline_arguments):
         super(ConfigManager, self).__init__(configuration_object, configuration_file, pipeline_arguments)
