0bc7819702f8da3b162c6194d826d80792b21990
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-24 10:10:06 +0100

    module for trackhub related exceptions

---
+++ trackhub/exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 24-08-2017 10:09
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
