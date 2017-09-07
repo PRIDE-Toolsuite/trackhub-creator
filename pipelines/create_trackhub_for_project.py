881e3561cac2672351d6e0320645f1ceb44d02dc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-07 11:34:21 +0100

    pipeline documentation

--- pipelines/create_trackhub_for_project.py
+++ pipelines/create_trackhub_for_project.py
@@ -8,5 +8,21 @@
 # 
 
 """
-Document your module here
+This pipeline creates a trackhub for a PRIDE project, based on the information provided via a JSON formatted file, as it
+can be seen on this sample:
+{
+  "trackHubName" : "PXD000625",
+  "trackHubShortLabel" : "<a href=\"http://www.ebi.ac.uk/pride/archive/projects/PXD000625\">PXD000625</a> - Hepatoc...",
+  "trackHubLongLabel" : "Experimental design For the label-free ...",
+  "trackHubType" : "PROTEOMICS",
+  "trackHubEmail" : "pride-support@ebi.ac.uk",
+  "trackHubInternalAbsolutePath" : "/nfs/pride/prod/archive/2014/05/PXD000625/internal/trackHub",
+  "trackMaps" : [ {
+    "trackName" : "PXD000625_10090_Original",
+    "trackShortLabel" : "<a href=\"http://www.ebi.ac.uk/pride/archive/projects/PXD000625\">PXD000625</a> - Mus musc...",
+    "trackLongLabel" : "Experimental design For the label-free proteome analysis 17 mice were used composed of 5 ...",
+    "trackSpecie" : "10090",
+    "pogoFile" : "/nfs/pride/prod/archive/2014/05/PXD000625/internal/PXD000625-10090.pogo"
+  } ]
+}
 """
