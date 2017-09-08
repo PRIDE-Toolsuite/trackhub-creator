191d51cff43c2e6aa8c03bdb17ffb7fc11b35489
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-08 11:05:27 +0100

    _PROJECT_DATA_FILE_KEY_TRACKHUB_SECTION_TRACKMAPS key

--- pipelines/create_trackhub_for_project.py
+++ pipelines/create_trackhub_for_project.py
@@ -92,6 +92,7 @@ class ProjectDescriptor:
     _PROJECT_DATA_FILE_KEY_TRACKHUB_HUB_TYPE = 'trackHubType'
     _PROJECT_DATA_FILE_KEY_TRACKHUB_EMAIL = 'trackHubEmail'
     _PROJECT_DATA_FILE_KEY_TRACKHUB_INTERNAL_ABSOLUTE_PATH = 'trackHubInternalAbsolutePath'
+    _PROJECT_DATA_FILE_KEY_TRACKHUB_SECTION_TRACKMAPS = 'trackMaps'
 
     def __init__(self, project_data_file_path):
         self.__project_data_file_path = project_data_file_path
