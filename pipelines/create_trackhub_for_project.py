e3f8ef9f422edae73800697d1c31e41d0df4bf31
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-08 11:03:18 +0100

    _PROJECT_DATA_FILE_KEY_TRACKHUB_LONG_LABEL key

--- pipelines/create_trackhub_for_project.py
+++ pipelines/create_trackhub_for_project.py
@@ -88,6 +88,7 @@ class ProjectDescriptor:
     # Project Data File keys
     _PROJECT_DATA_FILE_KEY_TRACKHUB_NAME = 'trackHubName'
     _PROJECT_DATA_FILE_KEY_TRACKHUB_SHORT_LABEL = 'trackHubShortLabel'
+    _PROJECT_DATA_FILE_KEY_TRACKHUB_LONG_LABEL = 'trackHubLongLabel'
 
     def __init__(self, project_data_file_path):
         self.__project_data_file_path = project_data_file_path
