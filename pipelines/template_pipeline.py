98bd5b0265659e768d869f99fc84e29bf59759ea
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-10 05:57:39 +0100

    PogoBasedPipeline superclass

--- pipelines/template_pipeline.py
+++ pipelines/template_pipeline.py
@@ -159,6 +159,9 @@ class Director:
         self.__logger = new_logger
         return self._get_logger()
 
+class PogoBasedPipeline(Director):
+    pass
+
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
