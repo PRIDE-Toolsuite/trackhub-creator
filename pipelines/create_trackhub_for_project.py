e3dae3e6f01b18fdb819800ab8502f2c3fa9f573
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-11 10:21:38 +0100

    add_error_message placeholder

--- pipelines/create_trackhub_for_project.py
+++ pipelines/create_trackhub_for_project.py
@@ -190,6 +190,9 @@ class PipelineResult:
         self.error_messages = []
         self.hub_descriptor_file_path = ""
 
+    def add_error_message(self, error_message):
+        pass
+
 
 class TrackhubCreatorForProject(PogoBasedPipelineDirector):
     # TODO
