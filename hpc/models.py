03669e69983464d4b8dfb5c79383e05e398599a4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-11 11:14:35 +0100

    models placeholder for the hpc module

--- hpc/models.py
+++ hpc/models.py
@@ -1,13 +1,12 @@
 # 
 # Author    : Manuel Bernal Llinares
 # Project   : trackhub-creator
-# Timestamp : 11-09-2017 11:10
+# Timestamp : 11-09-2017 11:14
 # ---
 # © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
 # All rights reserved.
 # 
 
 """
-Configuration Manager for this HPC Module
+Document your module here
 """
-
