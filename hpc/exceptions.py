ec5d4b12a97f762acd42713f592e903691be086d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-11 11:39:33 +0100

    exceptions placeholder for hpc module

---
+++ hpc/exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 11-09-2017 11:39
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
