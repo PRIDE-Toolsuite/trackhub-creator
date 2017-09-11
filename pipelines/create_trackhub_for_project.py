bd0d248fa53adee4aebc53b5f7b1a693836d80ff
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-11 10:36:54 +0100

    Absolute file path to the folder that represents the running session of the pipeline

--- pipelines/create_trackhub_for_project.py
+++ pipelines/create_trackhub_for_project.py
@@ -190,6 +190,8 @@ class PipelineResult:
         self.error_messages = []
         self.success_messages = []
         self.hub_descriptor_file_path = ""
+        # Absolute file path to the folder that represents the running session of the pipeline
+        self.file_path_pipeline_session = ""
 
     def set_status_error(self):
         self.status = self._VALUE_STATUS_ERROR
