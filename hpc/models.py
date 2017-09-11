eadf50671e45d530a25c7c5687b1bd08c7ff7d98
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-11 12:02:02 +0100

    HpcService abstract superclass, constructor

--- hpc/models.py
+++ hpc/models.py
@@ -38,7 +38,10 @@ class HpcServiceFactory:
 
 
 class HpcService(metaclass=abc.ABCMeta):
-    pass
+    def __init__(self):
+        self._logger = config_manager \
+            .get_app_config_manager() \
+            .get_logger_for("{}.{}".format(__name__, type(self).__name__))
 
 
 class HpcServiceLsf:
