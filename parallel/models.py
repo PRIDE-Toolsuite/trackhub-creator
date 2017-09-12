367ad00b8c11b395755850ab3173331dac2c6127
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-12 16:11:03 +0100

    models placeholder for the parallelization module

---
+++ parallel/models.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 12-09-2017 16:10
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
