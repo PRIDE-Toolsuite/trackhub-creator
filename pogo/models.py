e88da5610a6ca9a155f551a4a7954b621a5e9039
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-12 09:59:45 +0100

    PogoRunnerHpc, placeholder

--- pogo/models.py
+++ pogo/models.py
@@ -111,5 +111,9 @@ class PogoRunnerLocalThread(PogoRunner):
     pass
 
 
+class PogoRunnerHpc(PogoRunner):
+    pass
+
+
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
