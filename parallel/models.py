74fd4f57bbb1f749967b52e37775430ba56a96a4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 12:11:49 +0100

    refactor log message

--- parallel/models.py
+++ parallel/models.py
@@ -71,7 +71,7 @@ class ParallelRunner(metaclass=abc.ABCMeta, threading.Thread):
     def get_stderr(self):
         # Never give it back until the runner is done with whatever it is doing
         if not self._done:
-            raise ParallelRunnerException("Parallel Runner ID '{}' is NOT DONE doing its job, "
+            raise ParallelRunnerException("Runner Thread ID '{}' is NOT DONE doing its job, "
                                           "thus 'stderr' is NOT AVAILABLE".format(threading.current_thread().getName()))
         return self._stderr
 
