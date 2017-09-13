a7583a391974ae612f0a5683aef2b5fe6a9bf77a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 12:17:40 +0100

    refactor log message

--- parallel/models.py
+++ parallel/models.py
@@ -47,7 +47,7 @@ class ParallelRunner(metaclass=abc.ABCMeta, threading.Thread):
         ...
 
     def run(self):
-        self._logger.debug("Runner Thread ID '{}' --- START ---".format(threading.current_thread().getName()))
+        self._logger.debug("--- START ---".format(threading.current_thread().getName()))
         try:
             self._run()
         finally:
