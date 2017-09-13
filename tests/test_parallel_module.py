c390a543615c00bf19a119b8b315f54ddcc4fb8b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 13:10:22 +0100

    unit tests for the parallelization module, placeholder

---
+++ tests/test_parallel_module.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 13-09-2017 13:10
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
