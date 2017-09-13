078d527776c69b0ac287caae8942b7c648d2e819
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 11:50:24 +0100

    exceptions for the parallelization module

---
+++ parallel/exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 13-09-2017 11:50
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
