f64863ea53cda03ac2dd546798af49c70a167601
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 11:52:20 +0100

    prevent the script from running in standalone mode

--- parallel/exceptions.py
+++ parallel/exceptions.py
@@ -18,3 +18,7 @@ from exceptions import AppException
 class ParallelRunnerException(AppException):
     def __init__(self, value):
         super().__init__(value)
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
