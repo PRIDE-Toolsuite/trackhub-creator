11f5975305773203b19affe1f64ae0948fc48f8a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-13 12:43:48 +0100

    CommandLineRunnerOnHpc

--- parallel/exceptions.py
+++ parallel/exceptions.py
@@ -30,5 +30,10 @@ class CommandLineRunnerAsThread(CommandLineRunnerException):
         super().__init__(value)
 
 
+class CommandLineRunnerOnHpc(CommandLineRunnerException):
+    def __init__(self, value):
+        super().__init__(value)
+
+
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
