067f15fb64e772d66756b85f06106cf820547ee9
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-14 15:21:32 +0100

    refactor

--- pipelines/pride_cluster_export.py
+++ pipelines/pride_cluster_export.py
@@ -506,7 +506,7 @@ class PrideClusterExporter(PogoBasedPipelineDirector):
         # Return the results for running PoGo for the given cluster-file-exporter result files
         return pogo_run_results
 
-    def __get_track_hub_builder(self, trackhub_descriptor):
+    def __get_trackhub_builder(self, trackhub_descriptor):
         # TODO - Implement more complex logic for instantiating a trackhub builder, e.g. setting up the trackhub
         # TODO - destination folder from whatever comes in as a command line parameter
         return trackhubs.SimpleTrackHubBuilder(trackhub_descriptor)
@@ -743,7 +743,7 @@ class PrideClusterExporter(PogoBasedPipelineDirector):
             self._get_logger().info("PoGo results obtained for #{} taxonomies".format(len(pogo_run_results)))
             # TODO - Convert files to BigBed format, this will be addressed in the future
             # Create trackhub structure
-            trackhub_builder = self.__get_track_hub_builder(self.__get_trackhub_descriptor())
+            trackhub_builder = self.__get_trackhub_builder(self.__get_trackhub_descriptor())
             self.__populate_assemblies(trackhub_builder, pogo_run_results)
             # Compute the destination folder for this trackhub (including the 'latest' link) and prepare Destination
             # folder for this trackhub
