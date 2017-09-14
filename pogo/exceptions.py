eaa8b1c9c730d16b084f5ad703871b92aa0058f8
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-14 07:10:04 +0100

    exceptions module placeholder

---
+++ pogo/exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 14-09-2017 7:09
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
