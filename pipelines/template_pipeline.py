582bcf90b9b807f2e52c90a55374f77d29316dde
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-14 15:37:26 +0100

    get a logger for TrackhubCreationDirector

--- pipelines/template_pipeline.py
+++ pipelines/template_pipeline.py
@@ -240,6 +240,11 @@ class TrackhubCreationDirector:
     This is the abstract base class for those pipelines devoted to the creation of a trackhub
     """
 
+    def __init__(self):
+        self._logger = config_manager \
+            .get_app_config_manager() \
+            .get_logger_for("{}.{}".format(__name__, type(self).__name__))
+
     def _get_track_hub_builder(self, trackhub_descriptor):
         # TODO - Implement more complex logic for instantiating a trackhub builder, e.g. setting up the trackhub
         return trackhubs.SimpleTrackHubBuilder(trackhub_descriptor)
