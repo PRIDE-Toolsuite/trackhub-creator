f17599a13fd874ff89de39662db5b11a14c3de88
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-15 09:57:46 +0100

    log start of the pipeline

--- scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
+++ scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
@@ -15,3 +15,7 @@ function mylogger() {
 
 # Command line parameters
 PROJECT_DESCRIPTION_DATA_FILE=$1
+
+mylogger "Starting trackhub export for PRIDE Project described at '${PROJECT_DESCRIPTION_DATA_FILE}'"
+mylogger "Current directory is $($PWD)"
+mylogger "<--- Running create_trackhub_for_project --->"
