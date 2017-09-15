2c4d20df3f1e8fe32c111dd65467cf0f7ad091b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-15 09:58:53 +0100

    pipeline launch command

--- scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
+++ scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
@@ -19,3 +19,4 @@ PROJECT_DESCRIPTION_DATA_FILE=$1
 mylogger "Starting trackhub export for PRIDE Project described at '${PROJECT_DESCRIPTION_DATA_FILE}'"
 mylogger "Current directory is $($PWD)"
 mylogger "<--- Running create_trackhub_for_project --->"
+time python_install/bin/python ./main_app.py -a project_data_file=${PROJECT_DESCRIPTION_DATA_FILE} create_trackhub_for_project
