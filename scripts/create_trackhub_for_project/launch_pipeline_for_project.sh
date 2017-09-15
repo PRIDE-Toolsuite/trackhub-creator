158b327a80c888489f083cc0d378acaf31be04e6
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-15 09:53:51 +0100

    launcher documentation

--- scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
+++ scripts/create_trackhub_for_project/launch_pipeline_for_project.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+# Create Trackhub for PRIDE Project pipeline launcher
+#   usage: launcher.sh <project_description_data.json>
+#
+# Manuel Bernal Llinares <mbdebian@gmail.com>
+
