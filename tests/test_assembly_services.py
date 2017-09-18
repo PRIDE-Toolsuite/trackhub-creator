9bd4a1bd5bfa234195110dc2ddf57c8c6be19c84
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 12:19:46 +0100

    unit test placeholder for assembly services

---
+++ tests/test_assembly_services.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 18-09-2017 12:19
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
