b744444ca6867599dbff83c0513e2e668d7fd461
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 11:34:12 +0100

    _MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBY_ACCESSION constant

--- toolbox/assembly.py
+++ toolbox/assembly.py
@@ -64,6 +64,7 @@ class MappingEntry:
     # Mapping keys
     _MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBLY_NAME = 'assembly_name'
     _MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBY_LEVEL = 'assembly_level'
+    _MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBY_ACCESSION = 'assembly_accession'
 
     def __init__(self, mapping_entry_object):
         self.mapping_entry_object = mapping_entry_object
