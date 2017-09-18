3ce1037d1ebf00fbbea8e6947522d7cbf9502a58
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 09:49:11 +0100

    assembly toolbox that will have assembly mapping models

---
+++ toolbox/assembly.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 18-09-2017 09:48
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
