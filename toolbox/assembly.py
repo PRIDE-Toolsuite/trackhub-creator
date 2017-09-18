c65455ae7ea3bf1389bffaea78480ec39d709fca
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 11:57:17 +0100

    get_ensembl_assembly_accession implementation

--- toolbox/assembly.py
+++ toolbox/assembly.py
@@ -85,7 +85,7 @@ class MappingEntry:
         return self.__get_value_for_key_or_default(self._MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBLY_LEVEL)
 
     def get_ensembl_assembly_accession(self):
-        pass
+        return self.__get_value_for_key_or_default(self._MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBLY_ACCESSION)
 
     def get_ensembl_assembly_id(self):
         pass
