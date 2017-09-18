1c78cac7a9ce2a13ff648096d4b003efd02b5821
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 12:33:45 +0100

    unit test for the hardcoded sample list of taxonomies

--- tests/test_assembly_services.py
+++ tests/test_assembly_services.py
@@ -14,11 +14,23 @@ Unit tests for assembly services
 import unittest
 # Application imports
 import ensembl.service
-import toolbox.assembly
+from toolbox.assembly import AssemblyMappingServiceFactory
 
 
 class TestAssemblyMappingServices(unittest.TestCase):
 
-    def setUp(self):
-        self.ensembl_service = ensembl.service.get_service()
+
+    def test_ensembl_species_has_mapping(self):
+        ensembl_service = ensembl.service.get_service()
+        sample_ncbi_taxonomies = [9606, 10090]
+        species_data_service = ensembl_service.get_species_data_service()
+        assembly_mapping_service = AssemblyMappingServiceFactory.get_assembly_mapping_service()
+        for taxonomy_id in sample_ncbi_taxonomies:
+            ensembl_species_data_entry = species_data_service.get_species_entry_for_taxonomy_id(taxonomy_id)
+            self.assertIsNotNone("NCBI Taxonomy ID #{} with accession '{}' Maps to UCSC Assembly name '{}'"
+                                 .format(taxonomy_id,
+                                         ensembl_species_data_entry.get_assembly_accession(),
+                                         assembly_mapping_service
+                                         .get_ucsc_assembly_for_ensembl_assembly_accession(
+                                             ensembl_species_data_entry.get_assembly_accession())))
 
