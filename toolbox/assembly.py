dda3ecfd7fa7cdf056069d1363e8a22510e14efb
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-18 11:57:44 +0100

    get_ensembl_assembly_id implementation

--- toolbox/assembly.py
+++ toolbox/assembly.py
@@ -88,7 +88,7 @@ class MappingEntry:
         return self.__get_value_for_key_or_default(self._MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBLY_ACCESSION)
 
     def get_ensembl_assembly_id(self):
-        pass
+        return self.__get_value_for_key_or_default(self._MAPPING_ENTRY_KEY_ENSEMBL_ASSEMBLY_ID)
 
     def get_ensembl_assembly_base_count(self):
         pass
