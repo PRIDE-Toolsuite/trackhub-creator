75498fdf0a6825e731b02bb96f5610439b9d98ed
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-22 07:19:37 +0100

    pogo runners will need to have those as well

--- pogo/models.py
+++ pogo/models.py
@@ -123,6 +123,8 @@ class PogoRunner(ParallelRunner):
         self.pogo_input_file = pogo_input_file
         self.protein_sequence_file_path = protein_sequence_file_path
         self.gtf_file_path = gtf_file_path
+        self._stdout = b' '
+        self._stderr = b' '
         self.__success = True
         self.__pogo_run_result = None
 
