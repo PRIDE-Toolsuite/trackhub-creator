03e3f02e433e5c87bc4d174535591d77f2d3694c
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-26 11:08:38 +0100

    ensembl data collector pipeline launcher, documentation

--- scripts/ensembl_data_collector/launch_pipeline_for_pride_taxonomies.sh
+++ scripts/ensembl_data_collector/launch_pipeline_for_pride_taxonomies.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+# Collect data from the latest Ensembl Release, pipeline launcher
+#   usage: launcher.sh <project_description_data.json>
+#
+# Manuel Bernal Llinares <mbdebian@gmail.com>
+
