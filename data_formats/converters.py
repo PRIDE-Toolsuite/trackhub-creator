716d03ded52d01efd7f337faf1c5061d87cad67d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-28 14:20:55 +0100

    converters module for data formats, placeholder

---
+++ data_formats/converters.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 28-09-2017 14:20
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
