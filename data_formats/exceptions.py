d356dca24ed4b311257ae24509b680fec3308cdc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-02 11:36:53 +0100

    exceptions placeholder for data converters module

---
+++ data_formats/exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 02-10-2017 11:36
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
