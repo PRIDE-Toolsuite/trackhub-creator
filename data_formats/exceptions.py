24e4d385c681d42d6f5b42b690587b6b42b1c49f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-02 11:42:15 +0100

    prevent script from running as not part of the pipeline

--- data_formats/exceptions.py
+++ data_formats/exceptions.py
@@ -32,3 +32,7 @@ class FileDataFormatConverterException(DataFormatConverterException):
 class BedToBigBedMultithreadedConverterException(FileDataFormatConverterException):
     def __init__(self, value):
         super().__init__(value)
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
