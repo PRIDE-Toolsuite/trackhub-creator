7cf4f20b4177d1d974b125336e22019ebc480f7b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 15:11:19 +0100

    UnknownBigDataFileType

--- trackhub/exceptions.py
+++ trackhub/exceptions.py
@@ -29,6 +29,10 @@ class TrackHubLocalFilesystemExporterException(TrackHubExporterException):
     def __init__(self, value):
         super().__init__(value)
 
+class UnknownBigDataFileType(AppException):
+    def __init__(self, value):
+        super().__init__(value)
+
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
