1828d9c7686898b49c7fafd9187e2bafeec1c05e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 13:48:01 +0100

    configuration manager module for data formats module

---
+++ data_formats/config_manager.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : trackhub-creator
+# Timestamp : 03-10-2017 13:47
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
