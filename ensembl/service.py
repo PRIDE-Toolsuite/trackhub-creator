3e0cbcf720d5692fcace85e2c8d12d8097a3f30a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 10:10:13 +0100

    get_ucsc_chromosome_sizes_for_taxonomy placeholder

--- ensembl/service.py
+++ ensembl/service.py
@@ -105,6 +105,10 @@ class Service:
             self.__species_data_service = SpeciesService(self.__request_species_data())
         return self.__species_data_service
 
+    def get_ucsc_chromosome_sizes_for_taxonomy(self, taxonomy_id):
+        # TODO
+        pass
+
 
 if __name__ == '__main__':
     print("ERROR: This script is part of a pipeline collection and it is not meant to be run in stand alone mode")
