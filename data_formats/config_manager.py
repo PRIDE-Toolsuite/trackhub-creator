e4134c699cc2efe2aa93737a488b27ca7ae12dbb
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 13:54:23 +0100

    configuration service singleton getter

--- data_formats/config_manager.py
+++ data_formats/config_manager.py
@@ -26,9 +26,16 @@ def set_configuration_file(config_file):
     return __configuration_file
 
 
+def get_configuration_service():
+    global __configuration_service
+    if not __configuration_service:
+        __configuration_service = ConfigurationService(config_manager.read_config_from_file(__configuration_file),
+                                                       __configuration_file)
+    return __configuration_service
+
+
 class ConfigurationService(config_manager.ConfigurationManager):
     def __init__(self, configuration_object, configuration_file):
         super().__init__(configuration_object, configuration_file)
-        self.logger = config_manager.get_app_config_manager()\
+        self.logger = config_manager.get_app_config_manager() \
             .get_logger_for("{}.{}".format(__name__, type(self).__name__))
-
