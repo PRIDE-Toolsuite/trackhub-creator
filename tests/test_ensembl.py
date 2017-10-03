074afc19b4ac82acd5853c22bfbe68ac11124409
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 13:35:08 +0100

    log message update

--- tests/test_ensembl.py
+++ tests/test_ensembl.py
@@ -47,7 +47,7 @@ class TestEnsemblService(unittest.TestCase):
         for taxonomy in taxonomy_ids:
             chromosome_sizes = ensembl.service.get_service().get_ucsc_chromosome_sizes_for_taxonomy(taxonomy)
             self.assertIsNotNone(chromosome_sizes, "We got chromosome sizes")
-            self.logger.debug("Chromosome sizes for taxonomy '{}' ---> '{}'".format(taxonomy, str(chromosome_sizes)))
+            self.logger.debug("UCSC Chromosome sizes for taxonomy '{}' ---> '{}'".format(taxonomy, str(chromosome_sizes)))
 
 
 if __name__ == '__main__':
