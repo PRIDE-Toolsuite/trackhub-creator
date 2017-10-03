3555f27ffac15e25ec43ae8636da05372c4bfa35
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-03 14:45:30 +0100

    get_conversion_output implementation, is not abstract any more

--- data_formats/converters.py
+++ data_formats/converters.py
@@ -33,9 +33,10 @@ class DataFormatConverter(ParallelRunner):
         self._stdout = []
         self._stderr = []
 
-    @abc.abstractmethod
     def get_conversion_output(self):
-        ...
+        if not self.is_done():
+            raise DataFormatConversionNotFinished("{} - NOT FINISHED YET".format(self._get_conversion_details))
+        return "\n".join(self._stdout)
 
     @abc.abstractmethod
     def get_conversion_output_error(self):
