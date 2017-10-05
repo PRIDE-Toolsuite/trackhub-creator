0591fb4f3d4a4f103054c6b2838a5b536e71a33f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-05 14:22:33 +0100

    include taxonomy ID for trackhub track

--- pipelines/template_pipeline.py
+++ pipelines/template_pipeline.py
@@ -330,11 +330,13 @@ class TrackhubCreationPogoBasedDirector(PogoBasedPipelineDirector, TrackhubCreat
             # Main .bed track
             bed_file_path = pogo_results[taxonomy].get_pogo_result_main_bed_file_path()
             trackhub_track_main = self._get_trackhub_track_for_taxonomy_id(taxonomy)
+            trackhub_track_main.taxonomy_id = taxonomy
             trackhub_track_main.set_big_data_url(bed_file_path)
             trackhub_track_main.set_type(bed_file_path)
             # Main .bed track with PTMs
             bed_ptm_file_path = pogo_results[taxonomy].get_pogo_result_main_ptm_bed_file_path()
             trackhub_track_main_with_ptm = self._get_trackhub_track_for_taxonomy_id(taxonomy)
+            trackhub_track_main_with_ptm.taxonomy_id = taxonomy
             trackhub_track_main_with_ptm.set_track("{} with PTMs".format(trackhub_track_main_with_ptm.get_track()))
             trackhub_track_main_with_ptm.set_big_data_url(bed_ptm_file_path)
             trackhub_track_main_with_ptm.set_type(bed_ptm_file_path)
