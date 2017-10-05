56bb60042a697e0b02541c113ecb7ee186c437d3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-05 14:29:36 +0100

    launch track big data file conversion

--- trackhub/models.py
+++ trackhub/models.py
@@ -218,12 +218,18 @@ class TrackHubLocalFilesystemExporter(TrackHubExporter):
                     # Default destination for the big data file is just copying it
                     destination_file_path = os.path.join(assembly_folder, big_data_file_name)
                     # if track type is BED, workout the destination file as bigbed and do not copy the data, convert it
-                    if track.get_type() == BaseTrack.TRACK_TYPE_BED:
+                    if (track.get_type() == BaseTrack.TRACK_TYPE_BED)\
+                            and track.taxonomy_id:
                         destination_file_path = \
                             os.path.join(assembly_folder,
                                          "{}.bb".format(big_data_file_name[:big_data_file_name.rfind(".")]))
                         # The new name for the big data file
                         big_data_file_name = os.path.basename(destination_file_path)
+                        # Convert the file
+                        converter = DataFormatConverterFactory.get_bed_to_bigbed_converter(track.taxonomy_id,
+                                                                                           track.get_big_data_url(),
+                                                                                           destination_file_path)
+                        converter.start()
                     else:
                         shutil.copy(track.get_big_data_url(), destination_file_path)
                     # TODO - update the big data url with either the copied file or the newly built .bb (bigBed) file
