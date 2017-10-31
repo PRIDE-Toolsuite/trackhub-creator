0d70a4d9bfcfbef28aaf1fe42af990038ba0fc35
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-31 14:14:53 +0000

    shell script documentation

--- scripts/publish_trackhub/publish_trackhub.sh
+++ scripts/publish_trackhub/publish_trackhub.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+# Publish a trackhub described by the given json formatted input file
+#   usage: launcher.sh <trackhub_registry_url> <trackhub_registry_username> <trackhub_registry_password> <trackhub_description_data.json>
+#
+# Manuel Bernal Llinares <mbdebian@gmail.com>
+
