80a3b0db2252eb924dd2c88e4ad210dc5be22623
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-31 14:26:24 +0000

    we don't need the URL right now

--- scripts/publish_trackhub/publish_trackhub.sh
+++ scripts/publish_trackhub/publish_trackhub.sh
@@ -17,12 +17,13 @@ function mylogger() {
 }
 
 # Command line parameters
-CMD_PARAM_TRACKHUB_REGISTRY_URL=$1
-CMD_PARAM_TRACKHUB_REGISTRY_USERNAME=$2
-CMD_PARAM_TRACKHUB_REGISTRY_PASSWORD=$3
-CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE=$4
+#CMD_PARAM_TRACKHUB_REGISTRY_URL=$1
+CMD_PARAM_TRACKHUB_REGISTRY_USERNAME=$1
+CMD_PARAM_TRACKHUB_REGISTRY_PASSWORD=$2
+CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE=$3
 
 mylogger "Publishing Trackhub described by '${CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE}'"
 mylogger "Current directory is $($PWD)"
 mylogger "<--- Running publish_trackhub --->"
-time python_install/bin/python ./main_app.py -a trackhub_registry_url=${CMD_PARAM_TRACKHUB_REGISTRY_URL},trackhub_registry_username=${CMD_PARAM_TRACKHUB_REGISTRY_USERNAME},trackhub_registry_password=${CMD_PARAM_TRACKHUB_REGISTRY_PASSWORD},trackhub_description=${CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE} publish_trackhub
+#time python_install/bin/python ./main_app.py -a trackhub_registry_url=${CMD_PARAM_TRACKHUB_REGISTRY_URL},trackhub_registry_username=${CMD_PARAM_TRACKHUB_REGISTRY_USERNAME},trackhub_registry_password=${CMD_PARAM_TRACKHUB_REGISTRY_PASSWORD},trackhub_description=${CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE} publish_trackhub
+time python_install/bin/python ./main_app.py -a trackhub_registry_username=${CMD_PARAM_TRACKHUB_REGISTRY_USERNAME},trackhub_registry_password=${CMD_PARAM_TRACKHUB_REGISTRY_PASSWORD},trackhub_description=${CMD_PARAM_TRACKHUB_DESCRIPTION_DATA_FILE} publish_trackhub
