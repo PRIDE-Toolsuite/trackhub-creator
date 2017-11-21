232f8d2a4c18131c393ef532a98ada9eb318e1cc
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-11-21 16:39:26 +0000

    TODO tag on extension for pogo mismatch option, pogo runner factory

--- pogo/models.py
+++ pogo/models.py
@@ -109,6 +109,7 @@ class PogoRunResult:
 class PogoRunnerFactory:
     @staticmethod
     def get_pogo_runner(ncbi_taxonomy_id, pogo_input_file, protein_sequence_file_path, gtf_file_path):
+        # TODO - Needs to be extended for abstracting from results files from '-mm' parameter use
         # TODO - In the future, more PoGo runners will be implemented
         return PogoRunnerLocalThread(ncbi_taxonomy_id, pogo_input_file, protein_sequence_file_path, gtf_file_path)
 
