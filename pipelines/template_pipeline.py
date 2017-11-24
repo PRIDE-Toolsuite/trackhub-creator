180f4aa533cee372b3a2bc21279c25f156552ba4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-11-24 09:34:41 +0000

    iterate over pogo run results, placeholder

--- pipelines/template_pipeline.py
+++ pipelines/template_pipeline.py
@@ -330,6 +330,8 @@ class TrackhubCreationPogoBasedDirector(PogoBasedPipelineDirector, TrackhubCreat
             #    .get_ucsc_assembly_for_ensembl_assembly_accession(ensembl_species_entry.get_assembly_accession())
             # TODO - Now you have to iterate over the pogo results associated with each taxonomy and create a couple of
             # TODO - tracks for each one
+            for pogo_run_result in pogo_results[taxonomy]:
+                pass
             # Main .bed track
             bed_file_path = pogo_results[taxonomy].get_pogo_result_main_bed_file_path()
             trackhub_track_main = self._get_trackhub_track_for_taxonomy_id(taxonomy)
